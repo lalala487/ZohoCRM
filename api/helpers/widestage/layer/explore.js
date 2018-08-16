7dc95acc57d9e3195e506c7312b6ea2300dde04b
Pavlo Zhukov <grodas.p35@gmail.com>
2018-08-16 14:34:06 +0300

    Move code to separate methods {@task HO-352}

--- api/helpers/widestage/layer/explore.js
+++ api/helpers/widestage/layer/explore.js
@@ -38,51 +38,14 @@ module.exports = {
     const dataLayer = await sails.helpers.widestage.layer.get(name);
     const _dataSource = await sails.helpers.widestage.datasource.get(dataLayer);
 
-    const collections = dataLayer.params.schema.reduce((carry, schema) => {
-      const {collectionID} = schema;
-      const columns = dataLayer.objects.reduce((carry, field) => {
-        if (field.collectionID === collectionID) {
-          carry.push(field);
-        }
-
-        return carry;
-      }, []);
-
-      const collection = {collectionID, columns, schema};
-      carry.push(collection);
-      return carry;
-    }, []);
+    const collections = prepareCollections(dataLayer);
 
     const dataSource = {
       datasourceID: _dataSource.id,
       params: _dataSource.params,
     };
 
-    for (let n in dataLayer.params.joins) {
-      for (let j in collections) {
-        if (dataLayer.params.joins[n].sourceCollectionID === collections[j].collectionID || dataLayer.params.joins[n].targetCollectionID === collections[j].collectionID) {
-          {
-            let theOther;
-
-            if (dataLayer.params.joins[n].sourceCollectionID === collections[j].collectionID) {
-              theOther = dataLayer.params.joins[n].targetCollectionID;
-            }
-
-            if (dataLayer.params.joins[n].targetCollectionID === collections[j].collectionID) {
-              theOther = dataLayer.params.joins[n].sourceCollectionID;
-            }
-
-            if (isTargetInvolved(collections, theOther)) {
-              if (!collections[j]['joins']) {
-                collections[j]['joins'] = [];
-              }
-
-              collections[j]['joins'].push(dataLayer.params.joins[n]);
-            }
-          }
-        }
-      }
-    }
+    processJoines(dataLayer, collections);
 
     const result = await sails.helpers.widestage.collections.process(dataSource, collections);
 
@@ -93,6 +56,52 @@ module.exports = {
 
 
 };
+
+function prepareCollections(dataLayer) {
+  return dataLayer.params.schema.reduce((carry, schema) => {
+    const {collectionID} = schema;
+    const columns = dataLayer.objects.reduce((carry, field) => {
+      if (field.collectionID === collectionID) {
+        carry.push(field);
+      }
+
+      return carry;
+    }, []);
+
+    const collection = {collectionID, columns, schema};
+    carry.push(collection);
+    return carry;
+  }, []);
+}
+
+
+function processJoines(dataLayer, collections) {
+  for (let n in dataLayer.params.joins) {
+    for (let j in collections) {
+      if (dataLayer.params.joins[n].sourceCollectionID === collections[j].collectionID || dataLayer.params.joins[n].targetCollectionID === collections[j].collectionID) {
+        {
+          let theOther;
+
+          if (dataLayer.params.joins[n].sourceCollectionID === collections[j].collectionID) {
+            theOther = dataLayer.params.joins[n].targetCollectionID;
+          }
+
+          if (dataLayer.params.joins[n].targetCollectionID === collections[j].collectionID) {
+            theOther = dataLayer.params.joins[n].sourceCollectionID;
+          }
+
+          if (isTargetInvolved(collections, theOther)) {
+            if (!collections[j]['joins']) {
+              collections[j]['joins'] = [];
+            }
+
+            collections[j]['joins'].push(dataLayer.params.joins[n]);
+          }
+        }
+      }
+    }
+  }
+}
 
 function isTargetInvolved(collections, theOtherID) {
   let found = false;
